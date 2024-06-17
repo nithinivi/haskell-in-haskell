049d28d7df28ca96b86b712154d71d41f62379e8
nithinivi <nithinnivi@gmail.com>
2024-06-18 01:28:25 +0530

    added tests

--- app/Main.hs
+++ app/Main.hs
@@ -4,5 +4,13 @@ import Example (example)
 import Ourlude
 
 
+data StagedError = StagedError String String
+
+stageEither :: Show e => String -> Either e a -> Either StaggedError a
+stageEither name m = case m of
+                          Left e -> (StagedError name (show e))
+                          Right a -> Right a
+                          
+
 main :: IO ()
 main = putStrLn <| example
